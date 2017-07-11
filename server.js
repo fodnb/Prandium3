5da05d19ee0d30db81867c54cce21727c9267bbd
fodnb <Daniel.Mcghee6813@gmail.com>
2017-07-11 16:16:24 -0400

    added mlab

--- server.js
+++ server.js
@@ -65,9 +65,21 @@ app.use(
 );
 
 
+var databaseUrl = 'mongodb://localhost/Prandium';
+
+if(process.env.MONGODB_URI){
+
+  mongoose.connect(process.env.MONGODB_URI)
+
+}else{
+  mongoose.connect(databaseUrl);
+}
+
+
+
 mongoose.Promise = global.Promise;
         
-  mongoose.connect("mongodb://127.0.0.1:27017/Prandium");
+  // mongoose.connect("mongodb://127.0.0.1:27017/Prandium");
         
  var db = mongoose.connection;
 
